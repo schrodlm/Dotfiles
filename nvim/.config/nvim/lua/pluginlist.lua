45a30ad10f1a702b82964e3e8a701518b7aa9181
Matěj Schrödl <matej.schrodl@gmail.com>
2024-03-05 20:53:23 +0100

    nvim/neogit

--- nvim/.config/nvim/lua/pluginlist.lua
+++ nvim/.config/nvim/lua/pluginlist.lua
@@ -54,7 +54,18 @@ return{
     'nvim-lualine/lualine.nvim',
     dependencies = {"nvim-tree/nvim-web-devicons"}
 },
+-- neogit
+{
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",         -- required
+    "sindrets/diffview.nvim",        -- optional - Diff integration
 
+    -- Only one of these is needed, not both.
+    "nvim-telescope/telescope.nvim", -- optional
+  },
+  config = true
+}
 
 }
 
