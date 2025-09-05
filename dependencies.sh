41b33922c7cc785293599658891a330aa7a61d5f
Matěj Schrödl <matej.schrodl@gmail.com>
2025-09-05 18:48:41 +0200

    Update configure script

--- dependencies.sh
+++ dependencies.sh
@@ -6,7 +6,12 @@ sudo apt install -y git tmux zsh i3wm i3blocks
 #TODO:
 #install neovim (build from source)
 
-
-#Clone zsh-syntax-highlighting
+#Configure terminal
+#1. Install a package manger for ZSH (oh-my-zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#2. Install used plugins 
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone "https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv.git" "$ZSH_CUSTOM/plugins/autoswitch_virtualenv"
+
 
